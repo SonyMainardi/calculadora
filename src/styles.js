d91cff1cba2c7bbd3c69c86cfcea36111332e3cc
p_921469 <sony.silva@camara.leg.br>
2024-04-08 14:18:01 -0300

    ajuste de cores

--- src/styles.js
+++ src/styles.js
@@ -11,7 +11,8 @@ export const Container = styled.div`
 
 export const Content = styled.div`
     width: 20%;
-    background-color: #FFFFFF;
+    background-color: #7FFFD4;
+    
 `;
 
 export const Row = styled.div`
