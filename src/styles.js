bd4a3f39b556e76c7630dd0e80b74911a2c1f676
p_921469 <sony.silva@camara.leg.br>
2024-04-08 14:49:48 -0300

    degrade

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: #B0E0E6; 
+    background: linear-gradient(to bottom, #B0E0E6, #6495ED); 
     display: flex;
     align-items: center;
     justify-content: center;
